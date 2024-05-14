3a186922796a1e4b06fc5ec1216855380cd5d0e2
judithmarg <judithmargaritapacollalli@gmail.com>
2024-05-14 19:40:24 -0400

    add some services for coach (search by username)

--- frontend/src/components/view/CoachForm.jsx
+++ frontend/src/components/view/CoachForm.jsx
@@ -8,24 +8,50 @@ const CoachForm = ({show, onClose}) => {
   const [password, setPassword] = useState("");
   const [role, setRole] = useState("");
   const [coach, setCoach] = useState("");  //nos interesa el id
+  const [coachId, setCoachId] = useState(""); //nos interesa el id
+
+  const encontrarIdCoach = async (coach) => {
+    try {
+      const response = await axios.get(`http://localhost:3000/users/${coach}`);
+      setCoachId(response.data.id);
+    }catch(error) {
+      alert('Error al encontrar el id del coach');
+      console.error('Error:', error);
+    }
+  }
+
+  const updateRole = async (id) => {
+    try {
+      const response = await axios.put(`http://localhost:3000/users/${id}`, {
+        role: "COACH",
+      });
+      console.log(response.data.message);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
 
   const registerAll = async (e) => {
     console.log(name, password, role, coach);
     e.preventDefault();
+    encontrarIdCoach(coach);
     //BUSCAR EL NUMERO DE ID DEL COACH EN BACKEND
+    ///verificar tokens
+    updateRole(coachId);
+    //verificar que el coach exista y hacerlo coach porque todos son users al principio
     try {
       const response1 = await axios.post('http://localhost:3000/users', {
         name: name,
         password: password,
         role: "USER",
-        coachId: coach,
+        coachId: coachId,
       });
       alert(response1.data.message);
       const response2 = await axios.post('http://localhost:3000/users', {
         name: name2,
         password: password,
         role: "USER",
-        coachId: coach,
+        coachId: coachId,
       });
       alert(response1.data.message);
       alert(response2.data.message);
@@ -49,8 +75,6 @@ const CoachForm = ({show, onClose}) => {
             <input className="mb-5 rounded px-3" type="text" placeholder="Ingresa el nombre de tu par" name="name2" required onChange={(e) => setName2(e.target.value)}/>
             <label htmlFor="password" className="text-indigo-300 mb-2 font-medium"><b>Contraseña</b></label>
             <input className="mb-5 rounded px-3" type="password" placeholder="Ingresa tu contraseña" name="password" required onChange={(e) => setPassword(e.target.value)} />
-            {/*<label htmlFor="role"><b>Rol</b></label>
-             <input type="text" placeholder="Ingresa tu rol" name="role" required onChange={(e) => setRole(e.target.value)} />*/}
             <label htmlFor="coach" className="text-indigo-300 mb-2 font-medium"><b>Coach</b></label> 
             <input className="mb-5 rounded px-3" type="text" placeholder="Ingresa el nombre de tu coach" name="coach" required onChange={(e) => setCoach(e.target.value)} />
             <div className ="w-min h-min bg-indigo-500 px-5 text-indigo-50 rounded-lg">
