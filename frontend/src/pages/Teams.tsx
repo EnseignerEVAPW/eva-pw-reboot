b747dbb01ecfafd22cc16034c5d6e6cbaa497eb8
judithMarg <judithmargaritapacollalli@gmail.com>
2024-05-26 23:04:52 -0400

    add async team in frontend

--- frontend/src/pages/Teams.tsx
+++ frontend/src/pages/Teams.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
 import InviteSearch from '../components/InviteSearch';
+import {useProfile} from '../services/useProfile';
+import axios from 'axios';
 
 const teams = [
-  { name: 'ejemplito', users: ['Lana1', 'Tomato2', 'Desire']},
+  { name: 'ejemplito', users: ['Lana1', 'Tomato2', 'Desire'] },
   { name: 'ejemplito2', users: ['Top12', 'Tomato2', 'Ejemplo2'] },
   { name: 'ejemplito3', users: ['Tomato2', 'Hola2', 'Desire'] },
 ];
@@ -10,21 +12,38 @@ const teams = [
 const header = 'text-base bg-[#141d30] py-2 text-center rounded-lg font-medium text-indigo-200'
 
 const Teams = () => {
-  const [allTeams, setAllTeams ] = useState([]);
-  useEffect(()=>{
-    
-  },[])
+  const [allTeams, setAllTeams] = useState([]);
+  const { id } = useProfile();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log('el id es', id);
+        if(id){
+          const response = await axios.get(`http://localhost:3000/team/getTeamsBelong/${id}`);
+          console.log(response);  
+          setAllTeams(response.data);
+        }
+        
+      } catch (error) {
+        console.error("El error es: ", error);
+      }
+    }
+    fetchData();
+  }, [id])
+
   return (
-    <div className="grid grid-rows-3 grid-cols-6 gap-4 m-10">
-      <div className="row-span-3 col-span-4">
+    <div className="grid grid-cols-3 gap-4 p-10 flex-wrap">
+      <div className="col-span-2">
         <div className='grid grid-cols-6 gap-4 mb-3'>
           <div className={header}>Nombre team</div>
-          <div className={`col-span-3 ${header}`}>Participantes</div>
+          <div className={`col-span-4 ${header}`}>Participantes</div>
+          <div className={header}>Ver mas</div>
         </div>
         {teams.map((team) => (
           <div key={team.name} className='grid grid-cols-6 gap-4 mb-2'>
-            <div className='text-base bg-indigo-200 py-2 text-center rounded-lg'>{team.name}</div>
-            <div className='col-span-3 text-base bg-indigo-200 py-2 text-center rounded-lg flex justify-center'>
+            <div className='flex items-center justify-center col-span-1 text-base bg-indigo-200 py-2 rounded-lg'>{team.name}</div>
+            <div className='col-span-4 text-base bg-indigo-200 py-2 text-center rounded-lg flex justify-center'>
               {team.users.map((user) => (
                 <div key={user} className='p-2'>
                   {user === 'Tomato2' ?
@@ -39,10 +58,13 @@ const Teams = () => {
                 </div>
               ))}
             </div>
+            <div className='flex items-center justify-center col-span-1 text-base bg-indigo-200 text-center rounded-lg'>
+              <button className='items-center h-auto bg-violet-500 hover:bg-violet-600 active:bg-violet-700 text-center px-4 rounded text-white'>Ver team</button>
+            </div>
           </div>
         ))}
       </div>
-      <div className="row-span-3 col-span-2">
+      <div className="col-span-1">
         <InviteSearch />
       </div>
     </div>
