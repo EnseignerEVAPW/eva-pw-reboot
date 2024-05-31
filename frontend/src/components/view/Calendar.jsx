8f85cbb3d191925ed164c85974a7c511e09bd829
judithMarg <judithmargaritapacollalli@gmail.com>
2024-05-31 03:47:55 -0400

    add calendar values hardcode year

--- frontend/src/components/view/Calendar.jsx
+++ frontend/src/components/view/Calendar.jsx
@@ -5,10 +5,14 @@ import '../../../public/styles.css'
 import axios from 'axios';
 
 function Calendar({name}) {
-  const endDate = new Date();
-  const startDate = new Date(new Date().setDate(endDate.getDate() - 300));
-  const [calendar, setCalendar] = useState([]);
+  const endDate = new Date('December 31, 2023');
+  const startDate = new Date('January 01, 2023');
+  const [calendar, setCalendar] = useState({});
+  const [max, setMax] = useState(0);
+  const [yearSelected, setYearSelected] = useState('2023');
   const [maxValue, setMaxValue] = useState(0);
+  const [heatmapData, setHeatmapData] = useState([]);
+  const [monthLabels, setMonthLabels] = useState([]);
   useEffect(() => {
     async function getActivityCalendar(handle) {
       try {
@@ -16,9 +20,6 @@ function Calendar({name}) {
         const submissions = response.data;
         const submissionDates = submissions.map(submission => new Date(submission.creationTimeSeconds * 1000));
         
-        console.log(submissionDates);
-        let counter = 0;
-        let counterday = 0;
         const calendar = submissionDates.reduce((acc, date) => {
           const year = date.getFullYear();
           const month = date.getMonth() + 1; // Los meses son indexados desde cero
@@ -26,20 +27,12 @@ function Calendar({name}) {
           
           if (!acc[year]) acc[year] = {};
           if (!acc[year][month]) acc[year][month] = [];
-          
-          //acc[year][month].push((day,0));
-          if(acc[year][month].includes(day)){
-            acc[year][month].push([day,counterday+1]);
-            counter= Math.max(counter,counterday+1);
-          }else{
-            acc[year][month].push([day,0]);
-          }
-          console.log('tip', counter);
+          acc[year][month].push(day);
           return acc;
         }, {});
-        
-        console.log(calendar);
+        console.log('vengo', calendar);
         setCalendar(calendar);
+        //valuesCalendar(calendar);
       } catch (error) {
         console.error('Error fetching user status data:', error);
       }
@@ -48,40 +41,58 @@ function Calendar({name}) {
     getActivityCalendar('JudithMPeach');
   }, []);
 
-  const generateHeatmapValues = (start, end) => {
+  useEffect(()=> {
+    setHeatmapData(generateHeatmapValues());
+    setMonthLabels(generateMonthLabels().reverse());
+  },[calendar]);
+
+  const generateHeatmapValues = () => {
     const values = [];
-    let currentDate = new Date(start);
-    const endDate = new Date(end);
+    let maximoo = 0;
+    let currentDate = new Date(`January 01, ${yearSelected}`);
+    const endDate = new Date(`December 31, ${yearSelected}`);
 
     while (currentDate <= endDate) {
+      const year = currentDate.getFullYear();
+      const month = currentDate.getMonth()+1;
+      const day = currentDate.getDate();
+
+      const repeat = calendar[year] && calendar[year][month]?
+      calendar[year][month].reduce((repeat, val) => {
+        return val === day ? repeat+1 : repeat;
+      }, 0)
+      : 0;
+      maximoo = Math.max(maximoo, repeat);
       values.push({
         date: currentDate.toISOString().split('T')[0],
-        count: Math.floor(Math.random() * 5),
+        count: repeat,
       });
       currentDate = new Date(currentDate.setDate(currentDate.getDate() + 1));
     }
+    console.log('mis values', values, 'maximo', maximoo);
+    setMax(maximoo);
     return values;
   };
 
-  const generateMonthLabels = (start, end) => {
-    const monthLabels = [];
-    let current = new Date(start);
-    const endDate = new Date(end);
+  const generateMonthLabels = () => {
+    const monthLabels2 = [];
+    let current = new Date(`January 01, ${yearSelected}`);
+    const endDate = new Date(`December 31, ${yearSelected}`);
 
     while (current <= endDate) {
       const month = current.toLocaleString('default', { month: 'short' });
       const year = current.getFullYear();
       const label = `${month} ${year}`;
-      if (!monthLabels.includes(label))
-        monthLabels.push(label);
+      if (!monthLabels2.includes(label))
+        monthLabels2.push(label);
       current = new Date(current.getFullYear(), current.getMonth() + 1, 1);
     }
 
-    return monthLabels.reverse();
+    return monthLabels2.reverse();
   };
 
-  const heatmapData = generateHeatmapValues(startDate, endDate);
-  const monthLabels = generateMonthLabels(startDate, endDate).reverse();
+  //const heatmapData = generateHeatmapValues();
+  
 
   return (
     <div className="calendar-heatmap-container">
@@ -99,13 +110,14 @@ function Calendar({name}) {
         classForValue={(value) => {
           if (!value || value.count === 0)
             return 'color-empty';
-
-          return `color-scale-${value.count}`;
+          const aux = parseInt(value.count*4/max);
+          console.log('hey', aux)
+          return `color-scale-${aux}`;
         }}
         showMonthLabels={false}
         tooltipDataAttrs={(value) => {
           return {
-            'data-tip': `${value.date} has count: ${value.count}`,
+            'data-tip': `${value.date} has count: ${parseInt(value.count*4/max)}`,
           };
         }}
       />
