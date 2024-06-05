075f4aa915b0a74b88bda9a8c05bbba8251ed149
judithMarg <judithmargaritapacollalli@gmail.com>
2024-06-04 21:27:56 -0400

    add frontend about chats and teams

--- frontend/src/pages/Timeline.jsx
+++ frontend/src/pages/Timeline.jsx
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import axios from 'axios';
 
 const Timeline = () => {
   const location = useLocation();
-  const { user } = location.state || { user: { name: 'Unknown', meetings: [] } };
+  const { team } = location.state || { team: { name: 'Unknown', meetings: [] } };
+  const [meetings, setMeetings] = useState([]);
+  useEffect(()=> {
+    const loadData = async () => {
+    try{
+      const response = await axios.get(`http://localhost:3000/training/teamsTraining/${team.id}`);
+      setMeetings(response.data);
+    }catch(error){
+      console.error('El error es: ', error);
+    }
+    }
+    loadData();
+  },[team.id]);
 
   return (
     <div className="px-4 sm:px-8 md:px-16 lg:px-32 xl:px-48 py-8 text-white bg-gradient-to-r from-gray-800 to-gray-900 min-h-screen">
-      <h1 className="text-4xl mb-8 font-bold text-center animate-fadeIn">Timeline - {user.name}</h1>
-      {user.meetings.map((meeting, index) => (
+      <h1 className="text-4xl mb-8 font-bold text-center animate-fadeIn">Timeline - {team.name}</h1>
+      {meetings.map((meeting) => (
         <div 
-          key={index} 
+          key={meeting.id} 
           className="bg-gray-800 p-6 rounded-lg mb-6 shadow-lg transform transition-all duration-300 hover:scale-102 hover:bg-gray-700"
         >
           <div className="flex mb-4">
             <div className="w-1/6 text-left text-gray-400">
-              <div className="text-lg font-semibold">{meeting.date}</div>
+              <div className="text-lg font-semibold">{meeting.creationDate.slice(0,10)}</div>
             </div>
             <div className="w-5/6 flex items-start justify-between">
               <div className="w-3/4 p-2 border border-gray-700 rounded-md h-48 overflow-y-auto bg-gray-900 text-sm">
-                <pre className="whitespace-pre-wrap leading-tight">{meeting.chat}</pre>
+                {meeting.chat.map((each)=> (
+                  <pre className='whitespace-pre-wrap leading-tight'>{each.name}: {each.content}</pre>
+                ))}
               </div>
               <div className="w-1/4 ml-4">
+                {/*aca las imagenes con image o si se cambia el nombre */}
                 <img src={meeting.image} alt="Imagen de la reunión" className="rounded-md h-48 w-full object-cover border border-gray-700" />
               </div>
             </div>
